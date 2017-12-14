de0f3eb23a8779cadc6774a3b497e03457ec4d36
redneb07 <meiershane07@gmail.com>
2017-12-14 08:19:24 -0600

    Add files via upload

---
+++ server/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "quackdb";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
